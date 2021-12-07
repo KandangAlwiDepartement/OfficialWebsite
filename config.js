793c880f085d0831e2836e12c11a43d4010a03ee
KandangAlwiDepartement <95683754+KandangAlwiDepartement@users.noreply.github.com>
2021-12-07 16:39:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "KandangAlwi";
+var server_ip = "kandangalwi.net";
+var server_port = 25565;
+var ip_copied = "IP copied!";
+
+var firefly_count = 20;
+
+staff("xLqcy_", "acc9f8ff-e4dc-478b-8070-e3814abb6a49", "Founder");
+staff("HnaRay", "6bdd4d26-6fc7-45e6-9889-d7df0892c866", "Founder");
+staff("Alwisusilo", "f5a10cb9-6988-4298-9129-3423b8961384", "Owner");
+staff("xDaPid_", "6e862db6-51f3-472c-a77d-06a3a1020949", "Owner");
+staff("Dev_Dio", "fec3c5d2-236a-495f-a8af-b3a5a44fd86b", "Main Developer");
+staff("DoScaff", "b44e2fa3-ca3d-4165-bad4-a59fcd02ec61", "Co-Owner");
+staff("Luckys", "88998046-b5d1-470f-81c3-2a1e541ce193", "Minigame Developer");
+staff("RakhaShi", "849554f9-f30d-45d1-b504-f8d63fa1e359", "Minigame Developer");
+staff("xNepcy", "983045ba-edf0-47de-b118-8e8759f2e708", "Server Management");
+staff("rajaopak", "85750130-ee4b-4bb4-a28b-c1125779a460", "Network Developer");
+staff("d0giee", "bf335977-0cc7-4644-9a92-060843431987", "Admin");
+staff("TheRazerr", "7770ad05-8e91-4bb0-a385-67b80defe38e", "Builder");
+staff("Klebb_", "32e7b0d1-44b7-4eae-a22d-712fb12a4da0", "Builder");
+
+vote("Minecraft MP", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraft-mp.com/server/287720/vote/")
